e8deb37aebc73f79aebce3e145365d487b739a21
thien3103ht <thien3103ht@gmail.com>
2025-05-09 15:17:05 +0700

    New project

---
+++ src/views/App.js
@@ -0,0 +1,13 @@
+
+import './App.scss';
+import Toggle from '../components/state/Toggle.js';
+
+function App() {
+  return (
+    <div>
+      <Toggle></Toggle>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/views/data.js
@@ -0,0 +1,25 @@
+export const YoutubeData = [
+    {
+        id: 1,
+        title: "Cách học reactjs",
+        content: "Nguyen KIm Thien đã đăng",
+        avatar: "https://i.pinimg.com/736x/67/d5/01/67d501a80937407b32ca57cab543ac12.jpg"
+    },{
+        id: 2,
+        title: "Cách học reactjs",
+        content: "Nguyen Dinh phi đã đăng",
+        avatar: "https://i.pinimg.com/736x/55/10/f0/5510f02bda4328fe01bc2e95ddc2f984.jpg"
+    },
+    {
+        id: 3,
+        title: "Cách học reactjs",
+        content: "Nguyen Anh Tu đã đăng",
+        avatar: "https://i.pinimg.com/736x/ac/65/5f/ac655fc14784a18b251341c653fd65f1.jpg"
+    },
+    {
+        id: 4,
+        title: "Cách học reactjs",
+        content: "Nguyen Kim Thang đã đăng",
+        avatar: "https://i.pinimg.com/736x/d0/c7/d9/d0c7d9f56d0db73e31be47913be1c4be.jpg"
+    }
+]
